0828a61f5367b6a2dbef31ea34d55ffe5c4e035f
JUAN CAMILO ZULUAGA CASTAÑO <111556574+JuanKmiloZ@users.noreply.github.com>
2026-02-16 11:41:09 -0500

    Add files via upload

---
+++ E3. Solucion - Cadena de texto.py
@@ -0,0 +1,10 @@
+# crear variable "cadena" que contenga el texto "esto es un texto de ejemplo"
+# following the position of each letter in the chain, search that values are x, y for to say"texto" 
+
+cadena="Esto es un texto de ejemplo"
+type(cadena)
+print(type(cadena))
+len(cadena)
+print(cadena)
+print(len(cadena))
+print((cadena[11:16]))
